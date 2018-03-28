ba5a2e2b848aa615f079f4a1abf2a51f98d322c5
Kevin Gill <hkevgill@hotmail.com>
2018-03-27 21:12:19 -0700

    Created routes and corresponding databaseService functions for basic CRUD

--- app/server/routes/api.js
+++ app/server/routes/api.js
@@ -4,5 +4,30 @@ module.exports = function(app, express) {
 
     var apiRouter = express.Router();
 
+    // get all contacts
+    apiRouter.get('/contacts', function(req, res) {
+        databaseService.getContactList(req, res);
+    });
+
+    // get specific contact
+    apiRouter.get('/contacts/:contactId', function(req, res) {
+        databaseService.getContact(req, res);
+    });
+
+    // update contact information
+    apiRouter.put('/contacts', function(req, res) {
+        databaseService.updateContact(req, res);
+    });
+
+    // delete contact
+    apiRouter.delete('/contacts/:contactId', function(req, res) {
+        databaseService.deleteContact(req, res);
+    });
+
+    // add new contact
+    apiRouter.post('/contacts', function(req, res) {
+        databaseService.addContact(req, res);
+    });
+
     return apiRouter;
 };
--- app/server/services/database-service.js
+++ app/server/services/database-service.js
@@ -18,5 +18,132 @@ var pool = new pg.Pool(config);
 
 // create the database table
 module.exports.create = function(req, res) {
+    pool.connect(function(err, client, done) {
 
+        client.query('CREATE TABLE Contacts(ID SERIAL PRIMARY KEY, Contact_Name VARCHAR(300), Contact_Email VARCHAR(80), Contact_Phone VARCHAR(20), Contact_Mailing_Address VARCHAR(300));', function(err, results) {
+
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+                return console.error('error running query', err);
+            }
+
+        });
+
+    });
+}
+
+// get a list of all contacts
+module.exports.getContactList = function(req, res) {
+    pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+
+        client.query('SELECT * FROM "Contacts"', function(err, result) {
+
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+                return console.error('error running query', err);
+            }
+
+            res.json(result);
+        });
+    });
+}
+
+// get a specific contact
+module.exports.getContact = function(req, res) {
+
+    var data = {};
+
+    pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+
+        client.query('SELECT * FROM "Contacts" e WHERE "ID" = $1::int', [req.params.contactId], function(err, result) {
+
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+                return console.error('error running query', err);
+            }
+
+            res.json(result);
+
+        });
+    });
+}
+
+// save new contact information
+module.exports.updateContact = function(req, res) {
+
+    pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+
+        req.body.When = moment(req.body.When).utc().unix();;
+
+        client.query('UPDATE "Contacts" SET "Contact_Name" = $1::text, "Contact_Email" = $2::text, "Contact_Phone" = $3::text, "Contact_Mailing_Address" = $4::text WHERE "ID" = $5::int RETURNING *', [req.body.Contact_Name, req.body.Contact_Email, req.body.Contact_Phone, req.body.Contact_Mailing_Address, req.body.ID], function(err, result) {
+
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+                return console.error('error running query', err);
+            }
+
+            res.json(result);
+        });
+    });
+}
+
+// delete contact
+module.exports.deleteContact = function(req, res) {
+
+    pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+
+        client.query('DELETE FROM "Contacts" WHERE "ID" = $1::int', [req.params.contactId], function(err, result) {
+
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+                return console.error('error running query', err);
+            }
+
+            res.json(result);
+        });
+    });
+}
+
+// add contact
+module.exports.addContact = function(req, res) {
+
+    pool.connect(function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+
+        var query = client.query('INSERT INTO "Contacts"("Contact_Name", "Contact_Email", "Contact_Phone", "Contact_Mailing_Address") VALUES($1::text, $2::text, $3::text, $4::text) RETURNING *', [req.body.Contact_Name, req.body.Contact_Email, req.body.Contact_Phone, req.body.Contact_Mailing_Address], function(err, result) {
+
+            //call `done()` to release the client back to the pool
+            done();
+
+            if(err) {
+                return console.error('error running query', err);
+            }
+
+            res.json(result);
+        });
+    });
 }
